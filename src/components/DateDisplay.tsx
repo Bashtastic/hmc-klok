ecd2e6f30871115750f5eb0913c6406f25b4572e
Bashtastic <35097629+Bashtastic@users.noreply.github.com>
2025-01-27 13:19:32 +0100

    tweede poging om datadisplay groter te maken

--- src/components/DateDisplay.tsx
+++ src/components/DateDisplay.tsx
@@ -10,13 +10,16 @@ interface DateDisplayProps {
 const DateDisplay = ({ date, moonPhase, moonDescription }: DateDisplayProps) => {
   return (
     <div className="flex items-center justify-center space-x-8">
-      <span className="text-foreground text-10xl">{moonDescription}</span>
-      <span className="text-foreground text-16xl">{moonPhase}</span>
-      <p className="text-10xl font-light tracking-wide text-foreground">
+      <span className="text-foreground" style={{ fontSize: "8rem" }}>{moonDescription}</span>
+      <span className="text-foreground" style={{ fontSize: "12rem" }}>{moonPhase}</span>
+      <p className="font-light tracking-wide text-foreground" style={{ fontSize: "8rem" }}>
         {format(date, "d MMMM yyyy", { locale: nl })}
       </p>
     </div>
   );
 };
+
+
+
 
 export default DateDisplay;
