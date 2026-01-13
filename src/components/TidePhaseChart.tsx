23882dff552552137bc24e1f773818d132df325f
Bashtastic <35097629+Bashtastic@users.noreply.github.com>
2026-01-13 15:45:17 +0100

    520

--- src/components/TidePhaseChart.tsx
+++ src/components/TidePhaseChart.tsx
@@ -123,7 +123,7 @@ const TidePhaseChart = ({ tideData }: TidePhaseChartProps) => {
                 style={{
                   transform: "rotate(-90deg)",
                   whiteSpace: "nowrap",
-                  left: "220%",
+                  left: "520%",
                   bottom: "4px",
                   transformOrigin: "left bottom",
                 }}
